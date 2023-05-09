3caea1243c5e971202dec2ab60a5c0366695ccac
Matias Cipriano <81716638+matiascipriano@users.noreply.github.com>
2023-05-09 19:21:47 -0300

    PEP 8

--- admin/src/web/exportaciones/socios_PDF.py
+++ admin/src/web/exportaciones/socios_PDF.py
@@ -11,6 +11,7 @@ class PDF(FPDF):
         self.cell(200, 40, "Listado de Socios", 0, 0, "R")
         self.ln(10)
 
+        
     def footer(self):
         self.set_y(-15)
         self.set_font("Arial", "I", 8)
@@ -29,12 +30,12 @@ def generar_PDF(data_socios):
     y_height = 5
     pdf.set_xy(0, 50)
     pdf.cell(15)
-    pdf.cell(30, y_height, "Nombre", border=1)
-    pdf.cell(30, y_height, "Apellido", border=1)
-    pdf.cell(25, y_height, "Dni", border=1)
-    pdf.cell(25, y_height, "Telefono", border=1)
-    pdf.cell(30, y_height, "Genero", border=1)
-    pdf.cell(40, y_height, "Direccion", border=1, ln=1)
+    pdf.cell(30, y_height, "Nombre", border = 1)
+    pdf.cell(30, y_height, "Apellido", border = 1)
+    pdf.cell(25, y_height, "Dni", border = 1)
+    pdf.cell(25, y_height, "Telefono", border = 1)
+    pdf.cell(30, y_height, "Genero", border = 1)
+    pdf.cell(40, y_height, "Direccion", border = 1, ln = 1)
     pdf.set_font("Arial", "", 12)
     for socio in data_socios:
         cant_socios += 1
@@ -42,13 +43,14 @@ def generar_PDF(data_socios):
             pdf.add_page()
             cant_socios = 0
         pdf.cell(5)
-        pdf.cell(30, y_height, socio["nombre"], border=1)
-        pdf.cell(30, y_height, socio["apellido"], border=1)
-        pdf.cell(25, y_height, socio["dni"], border=1)
-        pdf.cell(25, y_height, socio["telefono"], border=1)
-        pdf.cell(30, y_height, socio["genero"], border=1)
-        pdf.cell(40, y_height, socio["direccion"], border=1, ln=1)
-    response = make_response(pdf.output(dest="S").encode("latin-1"))
-    response.headers.set("Content-Disposition", "attachment", filename="socios" + ".pdf")
+        pdf.cell(30, y_height, socio["nombre"], border = 1)
+        pdf.cell(30, y_height, socio["apellido"], border = 1)
+        pdf.cell(25, y_height, socio["dni"], border = 1)
+        pdf.cell(25, y_height, socio["telefono"], border = 1)
+        pdf.cell(30, y_height, socio["genero"], border = 1)
+        pdf.cell(40, y_height, socio["direccion"], border = 1, ln = 1)
+    response = make_response(pdf.output(dest = "S").encode("latin-1"))
+    response.headers.set("Content-Disposition",
+                         "attachment", filename="socios" + ".pdf")
     response.headers.set("Content-Type", "application/pdf")
     return response
