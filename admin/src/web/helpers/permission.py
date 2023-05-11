e4c8b2b60c89a6c56c53b2e8bae6a1b723772cc2
Matias Cipriano <81716638+matiascipriano@users.noreply.github.com>
2023-05-11 18:45:04 -0300

    Update permission.py

--- admin/src/web/helpers/permission.py
+++ admin/src/web/helpers/permission.py
@@ -2,8 +2,7 @@ from src.core import usuarios, permisos
 
 
 def has_permission(user, permission):
-    """Este metodo valida si el usuario 
-    posee o no el permiso enviado por parametro"""
+    """Este metodo valida si el usuario posee o no el permiso enviado por parametro"""
     usuario = usuarios.buscar_usuario_email(user)
     permiso = permisos.buscar_permiso(permission)
     for rol in usuario.roles:
