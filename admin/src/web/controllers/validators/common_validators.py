eff53291b7746e37440f3dc2576266aba2da1ed2
Matias Cipriano <81716638+matiascipriano@users.noreply.github.com>
2023-05-11 18:54:39 -0300

    cambio idioma vars

--- admin/src/web/controllers/validators/common_validators.py
+++ admin/src/web/controllers/validators/common_validators.py
@@ -1,26 +1,26 @@
-ignored_keys = ["activar_pagos"]
+llaves_ignoradas = ["activar_pagos"]
 
 
-def dict_values_are_none(data):
-    for value in data.values():
+def valores_dicc_none(dato):
+    for value in dato.values():
         if value is None:
             return True
     return False
 
 
-def dict_values_are_empty(data):
+def valores_dicc_vacios(dato):
     """La key activar_pagos puede estar vacía."""
-    for key, value in data.items():
-        if value == "" and key not in ignored_keys:
+    for key, value in dato.items():
+        if value == "" and key not in llaves_ignoradas:
             return True
     return False
 
 
-def is_integer(dato):
+def es_entero(dato):
     return dato.lstrip("-").isdigit()
 
 
-def is_float(dato):
+def es_float(dato):
     try:
         float(dato)
     except ValueError:
@@ -28,20 +28,20 @@ def is_float(dato):
     return True
 
 
-def is_positive_float(dato):
+def es_float_positivo(dato):
     return float(dato) >= 0
 
 
-def validar_longitud(dato, lenght=500):
-    return len(dato) <= lenght
+def validar_longitud(dato, longitud=500):
+    return len(dato) <= longitud
 
 
-def valor_fuera_de_rango_float(dato, max_value=1_000_000):
-    return float(dato) > max_value
+def valor_fuera_de_rango_float(dato, valor_max=1_000_000):
+    return float(dato) > valor_max
 
 
-def valor_fuera_de_rango_entero(dato, max_value=1_000_000):
-    return int(dato) > max_value
+def valor_fuera_de_rango_entero(dato, valor_max=1_000_000):
+    return int(dato) > valor_max
 
 
 def valor_es_negativo_entero(costo):
