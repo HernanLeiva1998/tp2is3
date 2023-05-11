799c7e40472ccc099653216a970aa7f40ca1c888
Matias Cipriano <81716638+matiascipriano@users.noreply.github.com>
2023-05-11 19:56:15 -0300

    Update socios_PDF.py

--- admin/src/web/exportaciones/socios_PDF.py
+++ admin/src/web/exportaciones/socios_PDF.py
@@ -27,15 +27,15 @@ def generar_PDF(datos_socios):
     pdf.set_font("Arial", "B", 16)
     pdf.cell(40, 20, f"{datetime.now().date()}")
     pdf.set_font("Arial", "B", 12)
-    y_height = 5
+    y_altura = 5
     pdf.set_xy(0, 50)
     pdf.cell(15)
-    pdf.cell(30, y_height, "Nombre", border = 1)
-    pdf.cell(30, y_height, "Apellido", border = 1)
-    pdf.cell(25, y_height, "Dni", border = 1)
-    pdf.cell(25, y_height, "Telefono", border = 1)
-    pdf.cell(30, y_height, "Genero", border = 1)
-    pdf.cell(40, y_height, "Direccion", border = 1, ln = 1)
+    pdf.cell(30, y_altura, "Nombre", border = 1)
+    pdf.cell(30, y_altura, "Apellido", border = 1)
+    pdf.cell(25, y_altura, "Dni", border = 1)
+    pdf.cell(25, y_altura, "Telefono", border = 1)
+    pdf.cell(30, y_altura, "Genero", border = 1)
+    pdf.cell(40, y_altura, "Direccion", border = 1, ln = 1)
     pdf.set_font("Arial", "", 12)
     for socio in datos_socios:
         cant_socios += 1
@@ -43,12 +43,12 @@ def generar_PDF(datos_socios):
             pdf.add_page()
             cant_socios = 0
         pdf.cell(5)
-        pdf.cell(30, y_height, socio["nombre"], border = 1)
-        pdf.cell(30, y_height, socio["apellido"], border = 1)
-        pdf.cell(25, y_height, socio["dni"], border = 1)
-        pdf.cell(25, y_height, socio["telefono"], border = 1)
-        pdf.cell(30, y_height, socio["genero"], border = 1)
-        pdf.cell(40, y_height, socio["direccion"], border = 1, ln = 1)
+        pdf.cell(30, y_altura, socio["nombre"], border = 1)
+        pdf.cell(30, y_altura, socio["apellido"], border = 1)
+        pdf.cell(25, y_altura, socio["dni"], border = 1)
+        pdf.cell(25, y_altura, socio["telefono"], border = 1)
+        pdf.cell(30, y_altura, socio["genero"], border = 1)
+        pdf.cell(40, y_altura, socio["direccion"], border = 1, ln = 1)
     respuesta = make_response(pdf.output(dest = "S").encode("latin-1"))
     respuesta.headers.set("Content-Disposition",
                          "attachment", filename="socios" + ".pdf")
