781d25fd9a2b1fd0a029879f5ac412964025a241
Matias Cipriano <81716638+matiascipriano@users.noreply.github.com>
2023-05-11 18:43:44 -0300

    borro cambios

--- admin/src/web/decorators/login.py
+++ admin/src/web/decorators/login.py
@@ -4,8 +4,7 @@ from flask import session, abort
 
 
 def login_requerido(f):
-    """Esta decorador es utilizado para verificar
-    si hay una sesion existente del usuario"""
+    """Esta decorador es utilizado para verificar si hay una sesion existente del usuario"""
 
     @wraps(f)
     def decorated_function(*args, **kwargs):
