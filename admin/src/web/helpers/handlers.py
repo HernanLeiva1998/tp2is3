cef81c8ffefdb08f7508a6d926c2673550250256
Matias Cipriano <81716638+matiascipriano@users.noreply.github.com>
2023-05-11 18:44:51 -0300

    correciones

--- admin/src/web/helpers/handlers.py
+++ admin/src/web/helpers/handlers.py
@@ -7,7 +7,7 @@ def not_found_error(e):
         "error_description": "La URL a la que intenta acceder no existe",
         "redirect_to": "home",
         "destino": "homepage",
-     }
+    }
     return render_template("error.html", **kwargs), 404
 
 
@@ -17,7 +17,7 @@ def not_authenticated_error(e):
         "error_description": "Debe autenticarse para acceder a esta URL",
         "redirect_to": "auth.login",
         "destino": "login",
-     }
+    }
     return render_template("error.html", **kwargs), 401
 
 
@@ -27,5 +27,5 @@ def not_authorized_error(e):
         "error_description": "No tiene los permisos para acceder a esta URL",
         "redirect_to": "home",
         "destino": "homepage",
-     }
+    }
     return render_template("error.html", **kwargs), 403
